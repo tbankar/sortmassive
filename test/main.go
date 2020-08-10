12c232e4775a1425adc073475bc608c1ddff17aa
tbankar <tabankar@gmail.com>
2020-08-10 17:05:12 +0530

    code to generate random numbers for testing purpose

---
+++ test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+        "math/rand"
+        "os"
+        "fmt"
+)
+
+// Main function
+func main() {
+
+        // Finding random numbers of int type
+        // Using Int() function
+        fp, _ := os.Create("input.txt")
+        defer fp.Close()
+        for i := 0; i < 50; i++ {
+                fp.WriteString(fmt.Sprintf("%d\n", rand.Int()))
+        }
+
+}
+
